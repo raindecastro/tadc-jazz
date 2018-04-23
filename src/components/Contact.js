3dc6425278fe87b10063315cfdc211a561e01d36
unknown <rainvincentdecastro@gmail.com>
2018-04-24 00:36:44 +0800

    removed blockchain

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,53 +1,57 @@
 import React from 'react';
-import addTimeIn from './connectblockchain'; 
-import FileBase64 from 'react-file-base64';
+// import addTimeIn from './connectblockchain'; 
+// import FileBase64 from 'react-file-base64';
 
-export default class Contact extends React.Component {
+// export default class Contact extends React.Component {
     
-    state = {
-        files: []
-    }
+//     state = {
+//         files: []
+//     }
 
-    handleAddOption = (e) => {
-        e.preventDefault();
+//     handleAddOption = (e) => {
+//         e.preventDefault();
 
-        let option = e.target.elements.alumni.value.trim();
+//         let option = e.target.elements.alumni.value.trim();
         
-        const value = addTimeIn(option, this.state.files[0].base64);
-        console.log(value);
-    };
+//         const value = addTimeIn(option, this.state.files[0].base64);
+//         console.log(value);
+//     };
 
-    getFiles = (files) => {
-        this.setState({ files: files })
-        console.log(files);
+//     getFiles = (files) => {
+//         this.setState({ files: files })
+//         console.log(files);
 
-        console.log(this.state.files[0].base64);
-    }
+//         console.log(this.state.files[0].base64);
+//     }
 
-    render() {
-        return (
-            <div>
-                <h1>Contact Me</h1>
-                <p>reach me at kyle.cnate@gmail.com</p>
-                <form className="add-option" onSubmit={this.handleAddOption}>
-                <input className="input" type="text" name="alumni"/>
-                <button className="button">Send</button>
-                </form>
-                <FileBase64 multiple={ true } onDone={ this.getFiles.bind(this) } />
-                {
-                    this.state.files.map((file, index) => (
-                    <img
-                        key={index}
-                        src={file.base64}
-                    />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Contact Me</h1>
+//                 <p>reach me at kyle.cnate@gmail.com</p>
+//                 <form className="add-option" onSubmit={this.handleAddOption}>
+//                 <input className="input" type="text" name="alumni"/>
+//                 <button className="button">Send</button>
+//                 </form>
+//                 <FileBase64 multiple={ true } onDone={ this.getFiles.bind(this) } />
+//                 {
+//                     this.state.files.map((file, index) => (
+//                     <img
+//                         key={index}
+//                         src={file.base64}
+//                     />
+//                     ))
+//                 }
+//             </div>
+//         );
+//     }
+// }
 
-// const Contacts = () => (
-    
-// );
+const Contacts = () => (
+    <div>
+        <h1>Contact Me</h1>
+        <p>reach me at kyle.cnate@gmail.com</p>
+    </div>
+);
 
+export default Contacts;
--- src/components/connectblockchain.js
+++ src/components/connectblockchain.js
@@ -1,40 +1,40 @@
 // web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 // web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io'"));
-window.web3 = new Web3(window.web3 ? window.web3.currentProvider : new Web3.providers.HttpProvider('https://ropsten.infura.io'))
-const abi = JSON.parse(
-  '[{"constant":false,"inputs":[{"name":"xname","type":"string"},{"name":"xtimeIn","type":"string"},{"name":"xtimeOut","type":"string"}],"name":"addTimeIn","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"xhash","type":"bytes32"},{"name":"ytimeOut","type":"string"}],"name":"updateTimeOut","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"xhash","type":"bytes32"}],"name":"getTime","outputs":[{"name":"xname","type":"string"},{"name":"xtimeIn","type":"string"},{"name":"xtimeOut","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]'
-);
+// window.web3 = new Web3(window.web3 ? window.web3.currentProvider : new Web3.providers.HttpProvider('https://ropsten.infura.io'))
+// const abi = JSON.parse(
+//   '[{"constant":false,"inputs":[{"name":"xname","type":"string"},{"name":"xtimeIn","type":"string"},{"name":"xtimeOut","type":"string"}],"name":"addTimeIn","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"xhash","type":"bytes32"},{"name":"ytimeOut","type":"string"}],"name":"updateTimeOut","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"xhash","type":"bytes32"}],"name":"getTime","outputs":[{"name":"xname","type":"string"},{"name":"xtimeIn","type":"string"},{"name":"xtimeOut","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]'
+// );
 
-const BlueBookContract = web3.eth.contract(abi);
-console.log(abi);
+// const BlueBookContract = web3.eth.contract(abi);
+// console.log(abi);
 
-//get from node using contractInstance.address
-const contractInstance = BlueBookContract.at(
-  '0x43746455e8e78EAfD0fe4267549857aCfFD5e669'
-);
+// //get from node using contractInstance.address
+// const contractInstance = BlueBookContract.at(
+//   '0x43746455e8e78EAfD0fe4267549857aCfFD5e669'
+// );
 
-function addTimeIn(personName, persontimeIn) {
+// function addTimeIn(personName, persontimeIn) {
  
-  // const persontimeIn = "8:00:00";
-  const persontimeOut = "18:00:00";
-  console.log(personName);
+//   // const persontimeIn = "8:00:00";
+//   const persontimeOut = "18:00:00";
+//   console.log(personName);
 
-  contractInstance.addTimeIn(personName,persontimeIn,persontimeOut,{from: web3.eth.accounts[0]},
-    function(error, result) {
-      //get hashed value from the blockchain
-      if (!error){
-        let hashed = contractInstance.addTimeIn.call(personName, persontimeIn, persontimeOut, function(error,result) {
-          if(!error) {
-            console.log(result)
-          }
-        });
-        console.log(result);
-      }
-    }
-  );
-}
+//   contractInstance.addTimeIn(personName,persontimeIn,persontimeOut,{from: web3.eth.accounts[0]},
+//     function(error, result) {
+//       //get hashed value from the blockchain
+//       if (!error){
+//         let hashed = contractInstance.addTimeIn.call(personName, persontimeIn, persontimeOut, function(error,result) {
+//           if(!error) {
+//             console.log(result)
+//           }
+//         });
+//         console.log(result);
+//       }
+//     }
+//   );
+// }
 
-export default addTimeIn;
+// export default addTimeIn;
 
 // function updateTimeOut(personName) {
 //   var currentHash;
